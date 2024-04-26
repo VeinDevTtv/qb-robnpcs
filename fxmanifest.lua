c9574ac62c7921c8bf798a27a75b1661969a5cc1
VeinDevTtv <142545968+VeinDevTtv@users.noreply.github.com>
2024-04-26 16:50:24 -0700

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'iiTzein'
+description 'Rob NPCs.'
+version '1.0.0'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
