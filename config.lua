534b6f8fdc981521b3adcdb3c7933e6f4ec67f1c
VeinDevTtv <142545968+VeinDevTtv@users.noreply.github.com>
2024-04-26 17:17:34 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -4,6 +4,6 @@ Config.minamount = 0
 
 Config.maxamount = 90
 
-Config.cooldown = 60000 -- how long the cooldown is between robbing NPCs. in milisecconds, so 30000 = 30 secconds
-
 Config.nomoneymsg = "This person is poor, just as you lol."
+
+Config.cooldown = 60000
